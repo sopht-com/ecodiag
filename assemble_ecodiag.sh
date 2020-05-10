8a415be17194173a80f8fe28e4ef61735cd4729f
Gael Guennebaud <gael.guennebaud@inria.fr>
2020-05-10 21:56:43 +0200

    add debug info

--- assemble_ecodiag.sh
+++ assemble_ecodiag.sh
@@ -3,5 +3,11 @@
 cp -R src/ public
 VERHASH=`git rev-parse --short HEAD`
 VERDATE=`git log -1 --pretty=format:"%ci" | cut -f 1 -d " "`
-sed -i "" "s/\%version_hash\%/$VERHASH/g" public/index.html
-sed -i "" "s/\%version_date\%/$VERDATE/g" public/index.html
+
+echo "VERHASH: $VERHASH"
+echo "VERDATE: $VERDATE"
+
+sed -i "" 's/\%version_hash\%/'$VERHASH'/g' public/index.html
+sed -i "" 's/\%version_date\%/'$VERDATE'/g' public/index.html
+
+grep "EcoDiag version" public/index.html
