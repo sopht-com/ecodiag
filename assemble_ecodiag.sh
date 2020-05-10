573d4b9aa14c92a08405c26ffc2c1164b88a98de
Gael Guennebaud <gael.guennebaud@inria.fr>
2020-05-10 21:40:31 +0200

    Add hash in version number

--- assemble_ecodiag.sh
+++ assemble_ecodiag.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cp -R src public
+cp -R src/ public
+VERHASH=`git rev-parse --short HEAD`
+VERDATE=`git log -1 --pretty=format:"%ci" | cut -f 1 -d " "`
+sed -i "" "s/\%version_hash\%/$VERHASH/g" public/index.html
+sed -i "" "s/\%version_date\%/$VERDATE/g" public/index.html
