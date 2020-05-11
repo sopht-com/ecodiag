16ba5e821a5dd1ab68cf88aa4c55979f38b433e0
Gael Guennebaud <gael.guennebaud@inria.fr>
2020-05-11 11:20:56 +0200

    add deployment of HEAD

--- assemble_ecodiag.sh
+++ assemble_ecodiag.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-cp -R src/ public
+TARGET=$1
+
+if [ ! -e public ]; then
+  mkdir public
+fi
+
+ls -R public/
+
+cp -R src/ $TARGET
 VERHASH=`git rev-parse --short HEAD`
 VERDATE=`git log -1 --pretty=format:"%ci" | cut -f 1 -d " "`
 
 echo "VERHASH: $VERHASH"
 echo "VERDATE: $VERDATE"
 
-
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  sed -i 's/\%version_hash\%/'$VERHASH'/g' public/index.html
-  sed -i 's/\%version_date\%/'$VERDATE'/g' public/index.html
+  sed -i 's/\%version_hash\%/'$VERHASH'/g' $TARGET/index.html
+  sed -i 's/\%version_date\%/'$VERDATE'/g' $TARGET/index.html
 else
-  sed -i "" 's/\%version_hash\%/'$VERHASH'/g' public/index.html
-  sed -i "" 's/\%version_date\%/'$VERDATE'/g' public/index.html
+  sed -i "" 's/\%version_hash\%/'$VERHASH'/g' $TARGET/index.html
+  sed -i "" 's/\%version_date\%/'$VERDATE'/g' $TARGET/index.html
 fi
 
-grep "EcoDiag version" public/index.html
+grep "EcoDiag version" $TARGET/index.html
