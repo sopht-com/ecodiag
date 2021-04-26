5fd1bbc77b289ec8b22a12f4f0699fb430abf31c
Gael Guennebaud <gael.guennebaud@inria.fr>
2021-04-26 10:20:48 +0200

    add fork-awesome fonts

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ export default {
 </script>
 
 <style>
+@import "./assets/css/icons.css";
 #app {
   /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   /* -webkit-font-smoothing: antialiased; */
