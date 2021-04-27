e4ce5af064e261b5a1927f8b4e12cea30dd6be11
Gael Guennebaud <gael.guennebaud@inria.fr>
2021-04-27 13:42:09 +0200

    fix nesting of b-select within a b-table

--- src/components/type_selector.vue
+++ src/components/type_selector.vue
@@ -19,7 +19,17 @@ import base_selector from './base_selector'
 export default {
   name: 'ecodiag-select-type',
   inheritAttrs: false,
-  mixins: [base_selector]
+  mixins: [base_selector],
+  updated() {
+    // This is required when nested wihtin a b-table:
+    // if a change of the b-select element trigger a change of the b-table,
+    // then this b-select element won't be properly updated
+    // (because the changes occurs during an @input event of the select element)
+    let self = this
+    this.$nextTick(function () {
+      self.selected = self.value
+    })
+  }
 }
 
 </script>
