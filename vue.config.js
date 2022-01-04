1ca34bd6068d2d6b5de891127336a92f10c7f9a8
Gael Guennebaud <gael.guennebaud@inria.fr>
2022-01-04 15:34:28 +0100

    enable single file i18n support

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.CI_PROJECT_NAME + process.env.ECODIAG_TARGET_PATH
-    : './'
+    : './',
+  chainWebpack: config => {
+    config.module
+      .rule('i18n')
+      .resourceQuery(/blockType=i18n/)
+      .type('javascript/auto')
+      .use('i18n')
+      .loader('@kazupon/vue-i18n-loader')
+  }
 }
