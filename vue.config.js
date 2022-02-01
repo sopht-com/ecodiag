8d181630a7555b1c74c0ef6290d8a3c03b7ee2f9
Gael Guennebaud <gael.guennebaud@inria.fr>
2022-02-01 15:13:28 +0100

    Add exemple.csv file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  assetsDir: 'static',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.CI_PROJECT_NAME + process.env.ECODIAG_TARGET_PATH
     : './',
