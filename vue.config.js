1a7e924bdf0d9fe1a3e45cbe5a284cfd1491eb8f
Gael Guennebaud <gael.guennebaud@inria.fr>
2022-02-01 15:35:46 +0100

    oops

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  assetsDir: 'static',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.CI_PROJECT_NAME + process.env.ECODIAG_TARGET_PATH
     : './',
